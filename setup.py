5ed8246b61052acf8d09b81bdc6d10af164dde4a
Jon Kracht <jonkracht@gmail.com>
2020-01-21 21:40:16 -0500

    Generated initial project from cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='jon_insight_project',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Jons Insight Project for 2020',
+    author='Jonathan Kracht',
+    license='BSD-3',
+)
